18981d3aa41dec2e934837971f6a928128f5bfd8
flikkes <lpke.flx@gmail.com>
2021-02-23 06:48:56 +0100

    added docs

--- src/main/java/com/felixdevelopment/db/MigrationStrategy.java
+++ src/main/java/com/felixdevelopment/db/MigrationStrategy.java
@@ -1,5 +1,28 @@
 package com.felixdevelopment.db;
 
+/**
+ * Describes how Data is written
+ */
 public enum MigrationStrategy {
-    REPLACE, MERGE, RENAME_OLD, RENAME_NEW;
+    /**
+     * If table or collection already exists, drop and replace it.
+     */
+    REPLACE,
+    /**
+     * Just add Data to collection/table if exists or create new collection/table if
+     * not. May produce duplicate data if same datasets were already present in
+     * collection/table.
+     */
+    MERGE,
+    /**
+     * If collection/table already exists and has data, rename it. Syntax:
+     * &lt;entity_name&gt;_OLD_&lt;System.currentTimeMillis()&gt;
+     */
+    RENAME_OLD,
+    /**
+     * If collection/table already exists and has data, leave it and choose an
+     * alternative collection/table name for the new data. Syntax:
+     * &lt;entity_name&gt;_NEW_&lt;System.currentTimeMillis()&gt;
+     */
+    RENAME_NEW;
 }
